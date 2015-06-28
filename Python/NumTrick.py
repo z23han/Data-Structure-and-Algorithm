f5493dca6b1088e9283bf2553b0ccad60b4f5731
Daniel Han <zxhan2012@gmail.com>
2015-06-28 14:02:06 -0400

    added some methods

--- Python/NumTrick.py
+++ Python/NumTrick.py
@@ -1,5 +1,8 @@
 # class wrapping all the possible methods manipulating numbers
 
+import math
+import random
+
 class NumTrick(object):
     def __init__(self, lst):
         self.lst = lst
@@ -96,15 +99,51 @@ class NumTrick(object):
         return countof2s
     
 
+# class wrapping string manipulations
+class StrTrick(object):
+    def __init__(self, str_in):
+        self.str_in = str_in
+        self.lst = str_in.split()
+
+    def shortest_dist_words(self, word1, word2):
+        dist1_lst = []
+        dist2_lst = []
+        cnt = 0
+        for ele in self.lst:
+            cnt += 1
+            if ele == word1:
+                dist1_lst.append(cnt)
+            elif ele == word2:
+                dist2_lst.append(cnt)
+        # create a dictionary to hold the pairs
+        dist_dict = {}
+        for v1 in dist1_lst:
+            for v2 in dist2_lst:
+                v = abs(v1 - v2)
+                dist_dict[v] = (v1, v2)
+        total_dist = dist_dict.keys()
+        v = sorted(total_dist)[0]
+        return [v, dist_dict[v]]
+    
 
 
 if __name__ == '__main__':
     K = 1
     lst = list(range(56))
-    #print 56
     num_trick = NumTrick(lst)
-    #print num_trick.countKs2(K)
-    #print num_trick.countKs(K)
-    print num_trick.count2sI(2224)
+    str_in = '''Use two loops: The outer loop picks all the elements
+    of arr[] one by one. The inner loop picks all the elements after
+    the element picked by outer loop. If the elements picked by outer
+    and inner loops have same values as x or y then if needed update
+    the minimum distance calculated so far.Use two loops: The outer
+    loop picks all the elements of arr[] one by one. The inner loop
+    picks all the elements after the element picked by outer loop.
+    If the elements picked by outer and inner loops have same values
+    as x or y then if needed update the minimum distance calculated
+    so far.'''
+    word1 = 'inner'
+    word2 = 'outer'
+    str_trick = StrTrick(str_in)
+    print str_trick.shortest_dist_words(word1, word2)
 
     
